e86e09916e6a9e8c076205368af6b18084e12eed
amritrajputt <vishal179singh@gmail.com>
2025-10-25 10:40:59 +0530

    solved part1 of week 2 assignment

--- assignments-main/assignments-main/week-2/week-2-js/hard/todo-list.js
+++ assignments-main/assignments-main/week-2/week-2-js/hard/todo-list.js
@@ -11,7 +11,38 @@
 */
 
 class Todo {
-
+  constructor(todo,index){
+    this.todo = [];
+    this.index = 0;
+  }
+  add(todo){
+      this.todo.push(todo)
+  }
+  remove(index){
+    if(index < 0 && index > this.todo.length) throw new Error ("Invalid index ...")
+      this.todo.splice(index,1)
+  }
+  update(index, updatedTodo){
+    if(index < 0 && index > this.todo.length) throw new Error ("Invalid index ...")
+      this.todo[index] = updatedTodo
+  }
+  getAll(){
+    return this.todo;
+  }
+  get(indexOfTodo){
+      if(indexOfTodo < 0 && indexOfTodo > this.todo.length) throw new Error ("Invalid index ...")
+        return this.todo[indexOfTodo]
+  }
+  clear(){
+    this.todo = []
+  }
 }
+const mytodo = new Todo
+mytodo.add("Go to the gym")
+mytodo.add("Solve Assignments")
+mytodo.remove(0)
+// mytodo.update(1,"Solve week 2 assignment")
+
+console.log(mytodo.getAll());
 
 module.exports = Todo;
