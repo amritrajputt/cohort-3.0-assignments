6754e477d10fa6a7e9b690f38b4eb3573d83366b
amritrajputt <vishal179singh@gmail.com>
2025-11-05 20:51:49 +0530

    week 4 assignment Q1 ✅

--- assignments-main/assignments-main/week-4/middlewares/01-ratelimitter.js
+++ assignments-main/assignments-main/week-4/middlewares/01-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req,res,next) => {
+  const userId = req.header('user-id')
+  if(!userId){
+    return res.status(400).json({ error: 'user-id header is required' });
+  }
+  if(!numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] = 1
+  }else{
+    numberOfRequestsForUser[userId]++
+  }
+  if(numberOfRequestsForUser[userId] > 5){
+    return res.status(404).json({ error: 'Rate limit exceeded' });
+  }
+  next()
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
