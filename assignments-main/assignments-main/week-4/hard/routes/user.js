b8a88745124748cea2a02ce933489ab26508075b
amritrajputt <vishal179singh@gmail.com>
2025-11-10 19:47:23 +0530

    assignments

--- assignments-main/assignments-main/week-4/hard/routes/user.js
+++ assignments-main/assignments-main/week-4/hard/routes/user.js
@@ -2,13 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
 
+
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+ 
 });
 
 router.post('/login', (req, res) => {
      // Implement user login logic
+   
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
